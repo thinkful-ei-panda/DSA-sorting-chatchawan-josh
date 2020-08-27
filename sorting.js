6d777f3705cdae42114907ec86ef2a41577fb576
jenthura <joshuasth@gmail.com>
2020-08-27 16:14:12 -0500

    not very cool bucket sort

--- sorting.js
+++ sorting.js
@@ -305,9 +305,50 @@ const sortLinkList = new LinkedList();
 sortLinkList.insertFirst(1);
 sortLinkList.insertLast(2);
 sortLinkList.insertLast(3);
-console.log(sortLinkList);
+// console.log(sortLinkList);
 
-console.log(mergeSort(sortLinkList)); // I wish
+//console.log(mergeSort(sortLinkList)); // I wish
+//We're skipping over question 5 in the interest of time. I'd rather complete 6-8 than take all our day up with 5.
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let temp = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + i] = temp;
+  }
+  return array;
+}
+
+function bucketSort(array) {
+  const n = array.length;
+  const allBuckets = new Array(n);
+  const sortedArray = [];
+
+  if (n < 2) {
+    return array;
+  }
+
+  for (let i = 0; i < n; i++) {
+    allBuckets[i] = [];
+  }
+
+  for (let i = 0; i < n; i++) {
+    const index = Math.floor((n * array[i]) / 10);
+    allBuckets[index].push(array[i]);
+  }
+
+  allBuckets.forEach((bucket) => {
+    insertionSort(bucket);
+    bucket.forEach((element) => sortedArray.push(element));
+  });
+  return sortedArray;
+}
+
+console.log('bucketSort: ', bucketSort([1, 3, 2, 1, 6, 8, 2, 4, 7, 9]));
 
 /*
 1) Suppose you are debugging a quicksort implementation that is supposed to sort an array in ascending order. After the first partition step has been completed, the contents of the array is in the following order: 3 9 1 14 17 24 22 20. Which of the following statements is correct about the partition step? Explain your answer. 
